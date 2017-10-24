76c897af763d5b550fcff83c9c032e384b798d4e
rkbhowmik <rajib.bhowmik@gmail.com>
2017-10-24 15:28:50 +0200

    -it will read a txt file with information of different accounts with different values
    - It will match regex pattern to find out account numbers and amount
    
    - Finally it will count amount to its respective account number

--- SIESummationApp/Program.cs
+++ SIESummationApp/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Globalization;
 
 namespace SIESummationApp
 {
@@ -10,6 +13,38 @@ namespace SIESummationApp
 	{
 		static void Main(string[] args)
 		{
+			Console.WriteLine("Write your SIE file with pathname:\n");
+			string SearchFile = Console.ReadLine();
+			StreamReader file = new StreamReader(SearchFile);
+
+			Dictionary<string, double> Account = new Dictionary<string, double>();
+
+			while (true)
+			{
+				var line = file.ReadLine();
+				if (line == null)
+					break;
+				string Pattern = @"#TRANS (\d{4}) {.*} (-?\d*)";
+				var match = Regex.Match(line, Pattern);
+				if (match.Success)
+				{
+					var accountID = match.Groups[1].Value;
+					var amount = float.Parse(match.Groups[2].Value, CultureInfo.InvariantCulture);
+
+					if (Account.ContainsKey(accountID))
+						Account[accountID] += amount;
+					else
+						Account[accountID] = amount;
+				}
+
+			}
+
+			foreach (var entry in Account.OrderBy(e => e.Key))
+			{
+				Console.WriteLine($"Account: {entry.Key} Total: {entry.Value.ToString()}\n");
+			}
+					Console.ReadLine();
+			
 		}
 	}
 }
